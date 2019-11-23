2b02329cf1766d8eaa5c1677c804480b94c157bb
Gustavo <graf.sousa@gmail.com>
2019-11-23 20:37:53 -0300

    Adiciona componente Header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import { Routes } from './routes';
 
+import { Header } from './components/Header/index';
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyle />
     </BrowserRouter>
