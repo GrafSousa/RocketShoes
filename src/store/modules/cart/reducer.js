60c111cfb84df1bbb55c4a341314cc984ac6cd83
Gustavo <graf.sousa@gmail.com>
2019-11-28 22:25:23 -0300

    Adiciona feature de verificar estoque na alteração

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -16,10 +16,7 @@ function cartReducer(state = [], action) {
           draft.splice(productIndex, 1);
         }
       });
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
 
